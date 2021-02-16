262f7a6fa36378a893211fe22027d334b1dc464e
Joss Saguira <josefina10969@gmail.com>
2021-02-15 20:38:43 -0600

    added hola file and the basic functions of a calculator

---
+++ hola.js
@@ -0,0 +1,39 @@
+'use-strcict';
+
+console.log("Elige una operación");
+console.log("1. Suma");
+console.log("2. Resta");
+console.log("3. Multiplicación");
+console.log("4. División");
+
+let opcion = 0;
+
+/* obtener numeros a partir del teclado */
+
+switch(opcion){
+    case 1:
+        suma(num1,num2);
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break; 
+}
+
+let suma = (numero1, numero2)=>{
+    console.log(numero1+numero2); 
+}
+
+let resta = (numero1, numero2)=>{
+    console.log(numero1-numero2); 
+}
+
+let multiplicacion = (numero1, numero2)=>{
+    console.log(numero1+numero2); 
+}
+
+let division = (numero1, numero2)=>{
+    console.log(numero1+numero2); 
+}
