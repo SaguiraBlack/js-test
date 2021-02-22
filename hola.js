293e9c4bfced75db32e52a46ee3fa8629bb650cf
Joss Saguira <josefina10969@gmail.com>
2021-02-21 20:59:43 -0600

    added pedirNumeros function and fixed some code

--- hola.js
+++ hola.js
@@ -3,37 +3,16 @@
 /* An alert that indicates the title of the program */
 alert("Calculadora sencilla");
 /* Initialized variable to confirm the repetition of the program */
-let repeticion=1;
+let repeticion = 1;
 
-while(repeticion==1){
-    /* Receiving the first number with a prompt */
-    let numero1 = window.prompt("Ingresa el primer número");
-    /* Parsing to number number1 */
-    numero1 = Number(numero1);
-    console.log(numero1);
-    /* Receiving the second number with a prompt */
-    let numero2 = window.prompt("Ingresa el segundo número");
-    /* Parsing to number number2 */
-    numero2 = Number(numero2);
-    console.log(numero2);
+let opcion = 0;
 
-    /* Options shown in console */
-    console.log("1. Suma");
-    console.log("2. Resta");
-    console.log("3. Multiplicación");
-    console.log("4. División");
+let numero1 = 0;
+let numero2 = 0;
 
-    /* Receiving the option to choose an operation with a prompt */
-    let opcion = prompt("Elige una operación\n1. Suma\n2. Resta \n3.Multiplicación\n4.División");
-    /* Parsing to number the option */
-    opcion = Number(opcion);
-    /* Initialized variable for the result */
-    let resultado = 0;
-    console.log(opcion);
+let resultado = 0;
 
-    /* Basic calcuator functions */
-    /* Sum */
-    let suma = (numero1, numero2) => resultado = numero1 + numero2;
+let suma = (numero1, numero2) => resultado = numero1 + numero2;
     /* Substraction */
     let resta = (numero1, numero2) => resultado = numero1 - numero2;
     /* Multiplication */
@@ -41,11 +20,78 @@ while(repeticion==1){
     /* Division */
     let division = (numero1, numero2) => resultado = numero1 / numero2;
 
+function mostrarMenu(opcion){
+    opcion = prompt("Elige una operación\n1. Suma\n2. Resta \n3.Multiplicación\n4.División");
+    opcion = Number(opcion);
+    return opcion;
+}
+
+function pedirNumeros(){
+    numero1 = window.prompt("Ingresa el primer número");
+    /* Parsing to number number1 */
+    numero1 = Number(numero1);
+    console.log(numero1);
+    /* Receiving the second number with a prompt */
+    numero2 = window.prompt("Ingresa el segundo número");
+    /* Parsing to number number2 */
+    numero2 = Number(numero2);
+    console.log(numero2);
+}
+
+function imprimirResultado(){
+    alert("El resultado es: "+resultado);
+    console.log(resultado);
+}
+
+while(repeticion === 1){
+
+    //let opcion = 0;
+
+    /*function mostrarMenu(opcion){
+         opcion = prompt("Elige una operación\n1. Suma\n2. Resta \n3.Multiplicación\n4.División");
+        return opcion;
+    }*/
+
+    
+
+    //mostrarMenu();
+    /* Receiving the first number with a prompt */
+    /*numero1 = window.prompt("Ingresa el primer número");
+    /* Parsing to number number1 */
+   // numero1 = Number(numero1);
+   // console.log(numero1);
+    /* Receiving the second number with a prompt */
+//    let numero2 = window.prompt("Ingresa el segundo número");
+    /* Parsing to number number2 */
+ //   numero2 = Number(numero2);
+  //  console.log(numero2);
+
+    /* Options shown in console */
+    
+
+    
+
+    /* Receiving the option to choose an operation with a prompt */
+    /*let opcion = prompt("Elige una operación\n1. Suma\n2. Resta \n3.Multiplicación\n4.División");*/
+    /* Parsing to number the option */
+
+    opcion = mostrarMenu(opcion);
+    pedirNumeros();
+
+    
+    /* Initialized variable for the result */
+    //let resultado = 0;
+    //console.log(opcion);
+
+    /* Basic calcuator functions */
+    /* Sum */
+    
+
     /* A function with an alert showing the result, in valid cases */
-    function imprimirResultado(){
+    /*function imprimirResultado(){
         alert("El resultado es: "+resultado);
         console.log(resultado);
-    }
+    }*/
 
     /* A switch case to do de operation based on the chosen option */
     switch(opcion){
@@ -70,7 +116,8 @@ while(repeticion==1){
             imprimirResultado();
             break;
         default:
-            alert("Opción no válida");
+            alert("Opción no válida. Elige una opción correcta.");
+            mostrarMenu();
             break;
     }
     
