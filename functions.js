14865dd75f4ae6a4e8bad1727c9d802f0dc20488
Joss Saguira <josefina10969@gmail.com>
2021-02-27 16:36:59 -0600

    Added a function to validate the value and cleaned the code

--- functions.js
+++ functions.js
@@ -18,20 +18,14 @@ let calculateCircleArea = (cicrcleRadius) => circleArea = pi * (cicrcleRadius **
 let calculateCirclePerimeter = (cicrcleRadius) => circlePerimeter = 2 * pi * cicrcleRadius;
 let calculateSquareArea = (squareSide) => squareArea = squareSide ** 2;
 let calculateSquarePerimeter = (squareSide) => squarePerimeter = 4 * squareSide;
-let calculateTriangleArea = (triangleBase, triangleHeight) => triangleArea = (triangleBase * triangleHeight) / 2;
+let calculateTriangleArea = (triangleBase) => triangleArea = (Math.sqrt(3) * (triangleBase ** 2) / 4);
 let calculateTrianglePerimeter = (triangleBase) => trianglePerimeter = 3 * triangleBase;
 
 /* FUNCTIONS */
 /* Calculating and showing the area and perimeter of a circle */
 function getCircleResults(){
     let cicrcleRadius = document.getElementById("circulo").value;
-    if(cicrcleRadius == 0){
-        alert(`El campo está vacío o ingresaste un 0`);
-    }
-    else if(cicrcleRadius < 0){
-        alert(`No se puede usar números negativos`);
-    }
-    else{
+    if(validateValue(cicrcleRadius)){
         calculateCircleArea(cicrcleRadius);
         calculateCirclePerimeter(cicrcleRadius);
         circleArea = circleArea.toFixed(5);    
@@ -43,45 +37,45 @@ function getCircleResults(){
 /* Calculating and showing the area and perimeter of a square */
 function getSquareResults(){
     let squareSide = document.getElementById("cuadrado").value;
-    if (squareSide == 0){
-        alert(`El campo está vacío o ingresaste un 0`);
-    }
-    else if(squareSide < 0){
-        alert(`No se puede usar números negativos`);
-    }
-    else{
+    if(validateValue(squareSide)){
         calculateSquareArea(squareSide);
         calculateSquarePerimeter(squareSide);
-        alert(`El área del cuadrado es ${squareArea}\nEl perímetro del cuadrado es ${squarePerimeter}`);
-    }  
+        alert(`El área del cuadrado es ${squareArea}\nEl perímetro del square es ${squarePerimeter}`);
+    } 
 }
 
 /* Calculating and showing the area and perimeter of a triangle */
 function getTriangleResults(){
     let triangleBase = document.getElementById("trianguloBase").value;
-    let triangleHeight = document.getElementById("trianguloAltura").value;
-    if(triangleBase == 0 || triangleHeight == 0){
-        alert(`Algún campo está vacío o ingresaste un 0`);
-    }
-    else if(triangleBase < 0 || triangleHeight < 0){
-        alert(`No se puede usar números negativos`);
-    }
-    else{
+    if(validateValue(triangleBase)){
         calculateTriangleArea(triangleBase, triangleHeight);
         calculateTrianglePerimeter(triangleBase);
         alert(`El área del triángulo es ${triangleArea}\nEl perímetro del triángulo es ${trianglePerimeter}`);
     }
 }
 
-function limpiarCampos(){
+/* A function to clean the inputs */
+function cleanFields(){
     document.getElementById("circulo").value = "";
     document.getElementById("cuadrado").value = "";
     document.getElementById("trianguloBase").value = "";
     document.getElementById("trianguloAltura").value = "";
 }
 
-
-
+/* A function to validate the value. Can't use negative numbers or an empty field */
+function validateValue(figureValue){
+    if(figureValue == 0){
+        alert(`El campo está vacío o ingresaste un 0`);
+        return false;
+    }
+    else if(figureValue < 0){
+        alert(`No se puede usar números negativos`);
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 
 /* encontrar coincidencias de una letra en una frase */
 
