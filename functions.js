4c57b0f99850cb44b48b5fe75f95daaf407fa41b
Joss Saguira <josefina10969@gmail.com>
2021-02-27 08:53:31 -0600

    added functions for square and triangle, and addes conditionals to validate the use of number. Added a function to clean the inputs

--- functions.js
+++ functions.js
@@ -1,41 +1,88 @@
 'use-strict'
 
 const pi = Math.PI;
-/* Initializating variables */
-let areaCirculo = 0;
-let perimetroCirculo = 0;
+/** Initializating variables **/
+/* Circle */
+let circleArea = 0;
+let circlePerimeter = 0;
+/* Square */
+let squareArea = 0;
+let squarePerimeter = 0;
+/* Triangle */
+let triangleArea = 0;
+let trianglePerimeter = 0;
 
-let calcularAreaCirculo = (radioCirculo) => areaCirculo = pi*(radioCirculo**2);
-let calcularPerimetroCirculo = (radioCirculo) => areaCirculo = 2*pi*radioCirculo;
+/* ARROW FUNCTIONS */
+/* Calculating areas and perimeters */
+let calculateCircleArea = (cicrcleRadius) => circleArea = pi * (cicrcleRadius ** 2);
+let calculateCirclePerimeter = (cicrcleRadius) => circlePerimeter = 2 * pi * cicrcleRadius;
+let calculateSquareArea = (squareSide) => squareArea = squareSide ** 2;
+let calculateSquarePerimeter = (squareSide) => squarePerimeter = 4 * squareSide;
+let calculateTriangleArea = (triangleBase, triangleHeight) => triangleArea = (triangleBase * triangleHeight) / 2;
+let calculateTrianglePerimeter = (triangleBase) => trianglePerimeter = 3 * triangleBase;
 
-function obtenerDatoArea(){
-    let radioCirculo = document.getElementById("circulo").value;
-    console.log(radioCirculo);
-    areaCirculo = calcularAreaCirculo(radioCirculo);
-    alert(`El área del círculo es ${areaCirculo}`);
+/* FUNCTIONS */
+/* Calculating and showing the area and perimeter of a circle */
+function getCircleResults(){
+    let cicrcleRadius = document.getElementById("circulo").value;
+    if(cicrcleRadius == 0){
+        alert(`El campo está vacío o ingresaste un 0`);
+    }
+    else if(cicrcleRadius < 0){
+        alert(`No se puede usar números negativos`);
+    }
+    else{
+        calculateCircleArea(cicrcleRadius);
+        calculateCirclePerimeter(cicrcleRadius);
+        circleArea = circleArea.toFixed(5);    
+        circlePerimeter = circlePerimeter.toFixed(5);
+        alert(`El área del círculo es ${circleArea}\nEl perímetro del círculo es ${circlePerimeter}`);
+    }
 }
 
-function obtenerDatoPerimetro(){
-    let radioCirculo = document.getElementById("circulo").value;
-    console.log(radioCirculo);
-    perimetroCirculo = calcularPerimetroCirculo(radioCirculo);
-    alert(`El perímetro del círculo es ${perimetroCirculo}`);
+/* Calculating and showing the area and perimeter of a square */
+function getSquareResults(){
+    let squareSide = document.getElementById("cuadrado").value;
+    if (squareSide == 0){
+        alert(`El campo está vacío o ingresaste un 0`);
+    }
+    else if(squareSide < 0){
+        alert(`No se puede usar números negativos`);
+    }
+    else{
+        calculateSquareArea(squareSide);
+        calculateSquarePerimeter(squareSide);
+        alert(`El área del cuadrado es ${squareArea}\nEl perímetro del cuadrado es ${squarePerimeter}`);
+    }  
+}
+
+/* Calculating and showing the area and perimeter of a triangle */
+function getTriangleResults(){
+    let triangleBase = document.getElementById("trianguloBase").value;
+    let triangleHeight = document.getElementById("trianguloAltura").value;
+    if(triangleBase == 0 || triangleHeight == 0){
+        alert(`Algún campo está vacío o ingresaste un 0`);
+    }
+    else if(triangleBase < 0 || triangleHeight < 0){
+        alert(`No se puede usar números negativos`);
+    }
+    else{
+        calculateTriangleArea(triangleBase, triangleHeight);
+        calculateTrianglePerimeter(triangleBase);
+        alert(`El área del triángulo es ${triangleArea}\nEl perímetro del triángulo es ${trianglePerimeter}`);
+    }
+}
+
+function limpiarCampos(){
+    document.getElementById("circulo").value = "";
+    document.getElementById("cuadrado").value = "";
+    document.getElementById("trianguloBase").value = "";
+    document.getElementById("trianguloAltura").value = "";
 }
 
 
 
 
-/* Calcular areas circulo, cuadrado, triandulo */
-
-
-/*function calcularArea(){
-    let radioCirculo = document.getElementById("circulo");
-    console.log(radioCirculo);
-    radioCirculo = Number(radioCirculo);
-    console.log(radioCirculo);
-    areaCirculo(radioCirculo);
-    alert(`El area del círculo es ${areaCirculo}`);
-}*/
 /* encontrar coincidencias de una letra en una frase */
 
 /* encontrar las vocales de una frase */
